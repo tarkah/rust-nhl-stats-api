08e9d68e2dfae7e16b70591a7a3462fadfe0aaf1
Cory Forsstrom <cforsstrom18@gmail.com>
2019-08-06 20:27:37 -0700

    remove clippy warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
+#![allow(clippy::redundant_field_names, clippy::new_without_default)]
+#![allow(clippy::len_zero, clippy::needless_return, clippy::redundant_closure)]
+#![allow(dead_code, non_camel_case_types)]
+
 #[macro_use]
 extern crate serde_derive;
 
+extern crate futures;
+extern crate hyper;
 extern crate serde;
 extern crate serde_json;
 extern crate url;
-extern crate hyper;
-extern crate futures;
 
 pub mod apis;
 pub mod models;
